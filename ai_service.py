9fbef668d887aa337f38456681707e6ba16d8a80
Kelooo0 <michockidominik1@gmail.com>
2026-05-29 21:35:37 +0200

    feat: add ai service logic

---
+++ ai_service.py
@@ -0,0 +1,25 @@
+from google import genai
+from google.genai import types
+import sys
+
+def ai_generate_commit(diff, API_KEY) -> str:
+    client = genai.Client(api_key=API_KEY)
+
+    config = types.GenerateContentConfig(
+        system_instruction="""
+                You are an expert software engineer. Analyze the provided git diff and generate
+                a short, concise commit message using Conventional Commits format (e.g., feat(auth): ..., fix(ui): ...).
+                Do not include any markdown bolding, quotes, code blocks, or explanations.
+                Return ONLY the raw commit message string so it can be directly piped into a command.
+            """,
+        temperature=0.2,
+    )
+    try:
+        response = client.models.generate_content(
+            model="gemini-2.5-flash", contents=diff, config=config
+        )
+        return response.text.strip()
+
+    except Exception as exc:
+        print("An error occured while generating AI response")
+        sys.exit(1)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from git_service import git_generate_commit
+from ai_service import ai_generate_commit
 from typing import Any
 from dotenv import load_dotenv
 import os
@@ -7,13 +8,13 @@ load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
 
-
 def main() -> Any:
     if not API_KEY:
         print("Error: Gemini api key is not set")
         print("Set it in you .env file based on the .env.example")
     print("Analyzing git changes")
     diff = git_generate_commit()
+    commit = ai_generate_commit(diff, API_KEY)
 
 
 if __name__ == "__main__":
